3f69c15461ce4bc59806cd83fb0bcd3363a3c517
Sparten-Ashvinee <ashvineek9@gmil.com>
2021-10-16 13:38:35 +0530

    Initialized DVC

---
+++ train.py
@@ -0,0 +1,181 @@
+import time
+import argparse
+import numpy as np
+import copy
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from utils import load_data, accuracy
+from models import GCN
+from onnix_food import export_onnix, ColaPredictor
+import wandb
+import os
+import hydra
+from omegaconf import OmegaConf
+
+
+
+run = wandb.init(project="Ferrato", tags=["GNN-food"])
+
+os.environ["WANDB_NOTEBOOK_NAME"] = "food_gnn_notebook"
+
+
+
+def check_cuda(cfg):
+    np.random.seed(cfg.training.seed)
+    torch.manual_seed(cfg.training.seed)
+    #cfg.training.cuda = not cfg.processing.no_cuda and torch.cuda.is_available()
+    if cfg.training.cuda:
+        torch.cuda.manual_seed(cfg.training.seed)
+
+
+
+def imp_cuda(cfg, model, adj, features, labels, idx_train, idx_val, idx_test):
+    if cfg.training.cuda:
+        model.cuda()
+        features = features.cuda()
+        adj = adj.cuda()
+        labels = labels.cuda()
+        idx_train = idx_train.cuda()
+        idx_val = idx_val.cuda()
+        idx_test = idx_test.cuda()
+
+
+def test(model, adj, features, labels, idx_test):
+    model.eval()
+    output = model(features, adj)
+    loss_test = F.nll_loss(output[idx_test[:354]], labels[idx_test[:354]])
+    acc_test = accuracy(output[idx_test[:354]], labels[idx_test[:354]])
+
+    wandb.log({"loss_test": loss_test.item()})
+    wandb.log({"acc_test": acc_test.item()})
+
+    print("Test set results:",
+          "loss= {:.4f}".format(loss_test.item()),
+          "accuracy= {:.4f}".format(acc_test.item()))
+
+
+# Train model
+def train(cfg, model, optimizer, scheduler, adj, features, labels, idx_train, idx_val):
+    t_total = time.time()
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    for epoch in range(cfg.training.epochs):
+        #train(epoch)
+        t = time.time()
+        model.train()
+        optimizer.zero_grad()
+        output = model(features, adj)
+        loss_train = F.nll_loss(output[idx_train], labels[idx_train])
+        acc_train = accuracy(output[idx_train], labels[idx_train])
+        loss_train.backward()
+        optimizer.step()
+        #scheduler.step()
+        if not cfg.training.fastmode:
+            # Evaluate validation set performance separately,
+            # deactivates dropout during validation run.
+            model.eval()
+            output = model(features, adj)
+
+        loss_val = F.nll_loss(output[idx_val], labels[idx_val])
+        acc_val = accuracy(output[idx_val], labels[idx_val])
+
+        if acc_val > best_acc:
+            best_acc = acc_val
+            best_model_wts = copy.deepcopy(model.state_dict())
+
+        wandb.log({"loss_train": loss_train.item()})
+        wandb.log({"acc_train": acc_train.item()})
+        wandb.log({"loss_val": loss_val.item()})
+        wandb.log({"acc_train": acc_val.item()})
+
+        print('Epoch: {:04d}'.format(epoch + 1),
+              'loss_train: {:.4f}'.format(loss_train.item()),
+              'acc_train: {:.4f}'.format(acc_train.item()),
+              'loss_val: {:.4f}'.format(loss_val.item()),
+              'acc_val: {:.4f}'.format(acc_val.item()),
+              'time: {:.4f}s'.format(time.time() - t))
+
+        time_elapsed = time.time() - t_total
+        print('Training complete in {:.0f}m {:.0f}s'.format(
+            time_elapsed // 60, time_elapsed % 60))
+        print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+
+    wandb.log({"total_time": (time.time() - t_total)})
+
+    return model, loss_train
+
+# Load data
+adj, features, labels, idx_train, idx_val, idx_test = load_data()
+adj = adj.coalesce().to_dense()
+
+
+@hydra.main(config_path="configs", config_name="config")
+def main(cfg):
+    print(torch.__version__)
+    # print(OmegaConf.to_yaml(cfg))
+    # wandb.config
+    wandb.config.update(cfg)
+
+    check_cuda(cfg)
+
+
+    # Model and optimizer
+    model = GCN(nfeat=features.shape[1],
+                nhid=cfg.training.hidden,
+                nclass=labels.max().item() + 1,
+                dropout=cfg.training.dropout)
+    optimizer = optim.Adam(model.parameters(),
+                           lr=cfg.training.lr, weight_decay=cfg.training.weight_decay)
+
+    imp_cuda(cfg, model, adj, features, labels, idx_train, idx_val, idx_test)
+
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+    model_ft, loss = train(cfg, model, optimizer, exp_lr_scheduler, adj, features, labels, idx_train, idx_val)
+
+
+    #Save model
+    path = ''
+    PATH = path+'checkpoints/model.pt'
+    '''
+    torch.save({
+                'epoch': cfg.training.epochs,
+                'model_state_dict': model_ft.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': loss,
+                }, PATH)
+    '''
+    print("Optimization Finished!")
+    #print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
+
+
+    # Testing
+    print('Testing the trained GNN model++++++++++++++++++++')
+    test(model, adj, features, labels, idx_test)
+
+    root_dir = 'onnix_model'
+
+
+    print('Exporting ONNIX model using Pytorch++++++++++++++')
+    export_onnix(root_dir, model, features, adj,path)
+
+
+    #ONNIX runtime
+    print('Runtime ONNIX model using Pytorch+++++++++++++++++')
+
+    aab = input().split(',')
+    predictions = ColaPredictor(path,root_dir, cfg.training.batch_size, cfg.training.max_length, features, adj).predict(PATH,model,aab)
+    print('Predictions: ', predictions)
+
+    print('Finished adding to WaB+++++++++++++++++++++++++++++')
+    run.finish()
+
+
+if __name__ =="__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,164 @@
+import copyreg
+import pickle
+import pandas as pd
+import numpy as np
+import scipy.sparse as sp
+import torch
+
+
+def encode_onehot(labels):
+    classes = set(labels)
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in
+                    enumerate(classes)}
+    labels_onehot = np.array(list(map(classes_dict.get, labels)),
+                             dtype=np.int32)
+    return labels_onehot
+
+
+def load_data(path="Data/", dataset="breakfast_ingredients_835_final6.csv"):
+    """Load citation network dataset (cora only for now)"""
+    print(path+dataset)
+    print('Loading {} dataset...'.format(dataset))
+
+    # idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset),
+    #                                     dtype=np.dtype(str))
+
+    ingg = pd.read_csv(path+'all_ingredients_name.csv')
+    col = list(ingg.In)
+    cols = ['Dish Type', 'Title'] + col
+
+    citations = pd.read_csv(
+        "Data/breakfast_ingredients_835_final6.csv",
+        sep=",",
+        header=None,
+        names=cols,
+    )
+
+
+    citations = citations.reset_index()
+    citations = citations.drop(citations.index[0])
+    citations = citations.drop('level_0', axis=1)
+    citations = citations.drop('level_1', axis=1)
+    citations = citations.drop('level_2', axis=1)
+    # citations = citations.drop('Dish Type', axis=1)
+
+    title = citations.Title.values
+    citations = citations.drop('Title', axis=1)
+
+    # citations.index = citations.index.astype('int')
+    cc = citations.to_numpy().astype(int)
+
+    features = sp.csr_matrix(cc, dtype=np.float32)
+    labels = encode_onehot(cc[:, 0])
+    #features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+    #labels = encode_onehot(idx_features_labels[:, -1])
+
+
+
+    # build graph
+    #idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
+    title = title.astype(int)
+    idx = np.array(title, dtype=np.int32)
+    idx_map = {j: i for i, j in enumerate(idx)}
+    # idx_map = {j: i for i, j in enumerate(idx)}
+    # edges_unordered = np.genfromtxt("{}{}.cites".format(path, dataset),
+    #                                 dtype=np.int32)
+    edges_unordered = pd.read_csv(
+        "Data/similar_ingredients.csv",
+        sep=",",
+        header=None,
+        names=['FTitle', 'STitle', 'Sting'],
+    )
+
+    edges_unordered = edges_unordered.reset_index()
+    edges_unordered = edges_unordered.drop('index', axis=1)
+    edges_unordered = edges_unordered.drop(index=0, axis=0)
+    edges_unordered = edges_unordered.to_numpy().astype(int)
+
+    edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
+                     dtype=np.int32).reshape(edges_unordered.shape)
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                        shape=(labels.shape[0], labels.shape[0]),
+                        dtype=np.float32)
+
+    # build symmetric adjacency matrix
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+    features = normalize(features)
+    adj = normalize(adj + sp.eye(adj.shape[0]))
+
+    idx_train = range(140)
+    idx_val = range(200, 500)
+    idx_test = range(500, 1500)
+
+    features = torch.FloatTensor(np.array(features.todense()))
+    labels = torch.LongTensor(np.where(labels)[1])
+    adj = sparse_mx_to_torch_sparse_tensor(adj)
+
+    idx_train = torch.LongTensor(idx_train)
+    idx_val = torch.LongTensor(idx_val)
+    idx_test = torch.LongTensor(idx_test)
+
+    #print(data)
+    print('==============================================================')
+
+    # Gather some statistics about the graph.
+    # print(f'Number of nodes: {data.num_nodes}')
+    # print(f'Number of edges: {data.num_edges}')
+    # print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
+    # # print(f'Number of training nodes: {data.train_mask.sum()}')
+    # # print(f'Training node label rate: {int(data.train_mask.sum()) / data.num_nodes:.2f}')
+    # print(f'Contains isolated nodes: {data.has_isolated_nodes()}')
+    # print(f'Contains self-loops: {data.has_self_loops()}')
+    # print(f'Is undirected: {data.is_undirected()}')
+
+    return adj, features, labels, idx_train, idx_val, idx_test
+
+
+def normalize(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
+
+
+def accuracy(output, labels):
+    preds = output.max(1)[1].type_as(labels)
+    correct = preds.eq(labels).double()
+    correct = correct.sum()
+    return correct / len(labels)
+
+
+def _sparse_tensor_constructor(indices, values, size):
+    return torch.sparse.FloatTensor(indices, values, size).coalesce()  # not coalesced from the default constructor
+
+
+def _reduce(x):
+    # dispatch table cannot distinguish between torch.sparse.FloatTensor and torch.Tensor (?)
+    if isinstance(x, torch.sparse.FloatTensor):
+        return _sparse_tensor_constructor, (x._indices(), x._values(), x.size())
+    else:
+        return torch.Tensor.__reduce_ex__(x, pickle.HIGHEST_PROTOCOL)  # use your own protocol
+
+
+class ExtendedPickler(pickle.Pickler):
+
+
+    dispatch_table = copyreg.dispatch_table.copy()
+    dispatch_table[torch.Tensor] = _reduce
+    # tried to use torch.sparse.FloatTensor instead of torch.Tensor but did not work (?)
+
+
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
+
